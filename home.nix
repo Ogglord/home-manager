f88532ff1fc68a3af3e26710442ed0919384a23d
Ogglord <oag@proton.me>
2024-10-26 22:43:18 +0200

    yet another home-manager repo

---
+++ home.nix
@@ -0,0 +1,98 @@
+{ config, pkgs, lib, ... }:
+let
+  inherit (pkgs.stdenv) isLinux isDarwin;
+  homeDir = if isDarwin then "/Users/" else "/home/";
+  username = if isDarwin then "oscaragren" else "ogge";
+  email = "oag@proton.me";
+in
+{
+
+  home = {
+    homeDirectory = homeDir + username;
+    inherit username;
+    stateVersion = "24.05";
+  };
+
+
+  home.packages = with pkgs; [
+    nixpkgs-fmt
+
+    (nerdfonts.override { fonts = [ "Hack" "Meslo" ]; })
+
+    # # You can also create simple shell scripts directly inside your
+    # # configuration. For example, this adds a command 'my-hello' to your
+    # # environment:
+     (writeShellScriptBin "help" ''
+       echo "Hello, ${config.home.username}!"
+     '')
+        bat
+        btop
+        byobu
+        comma # run any app without installing, prefix it with ","
+        eza
+        gh
+        git
+        fortune
+        #kmon # kernel module TUI
+        less
+        macchina # neofetch alternative in rust
+	      ncdu
+        nil # nix language interpreter
+        nixpkgs-fmt
+	      #powertop
+        rclone
+        rsync
+        smartmontools
+        #sysz # systemctl TUI
+        tmux
+        tree
+        zsh
+  ];
+
+  # Home Manager is pretty good at managing dotfiles. The primary way to manage
+  # plain files is through 'home.file'.
+  home.file = {
+    # # Building this configuration will create a copy of 'dotfiles/zshrc' in
+    # # the Nix store. Activating the configuration will then make '~/.zshrc' a symlink to the Nix store copy.
+    ".zshrc".source = dotfiles/zshrc;
+    ".zsh_aliases".source = dotfiles/zsh_aliases;
+
+    # # You can also set the file content immediately.
+    # ".gradle/gradle.properties".text = ''
+    #   org.gradle.console=verbose
+    #   org.gradle.daemon.idletimeout=3600000
+    # '';
+  };
+
+  # Home Manager can also manage your environment variables through
+  # 'home.sessionVariables'. These will be explicitly sourced when using a
+  # shell provided by Home Manager. If you don't want to manage your shell
+  # through Home Manager then you have to manually source 'hm-session-vars.sh'
+  # located at either
+  #
+  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
+  #
+  # or
+  #
+  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
+  #
+  # or
+  #
+  #  /etc/profiles/per-user/ogge/etc/profile.d/hm-session-vars.sh
+  #
+  home.sessionVariables = {
+     EDITOR = "micro";
+  };
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+
+  home.activation =
+    {
+      setupGitAuth = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+              	echo "Configuring git to use gh cli auth..."
+                export PATH="${pkgs.git}/bin:''$PATH"
+                ${pkgs.gh}/bin/gh auth setup-git
+      '';
+    };
+}
