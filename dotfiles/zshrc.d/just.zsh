6e413f3e2a92c42e6dbd60859e9690b3b170d958
Ogglord <oag@proton.me>
2024-11-03 23:05:32 +0100

    auto commit

--- dotfiles/zshrc.d/just.zsh
+++ dotfiles/zshrc.d/just.zsh
@@ -1,8 +1,3 @@
 # load our custom justfile completions
+. ~/.config/zsh/just.zsh || echo "just file missing!"
 
-if [ -e "~/.config/zsh/just.zsh" ]; then
-  . ~/.config/zsh/just.zsh
-  export ZSH_JUST_COMPLOAD=1
-else
-  export ZSH_JUST_COMPLOAD=0
-fi
